3c609ec91f410ff14784a884c6d45c4ce0ed17f7
NagKothapalli <nag022@gmail.com>
2022-09-21 08:18:26 +0530

    Sep21st code pushed

--- src/seleniumAutomation/GmailAutomation.java
+++ src/seleniumAutomation/GmailAutomation.java
@@ -3,10 +3,13 @@ package seleniumAutomation;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 public class GmailAutomation {
 	
 	//Selenium IDE - Tool / Plugin , WebDriver - jar file
 	//ChromeDriver , FiIrefoxDriver , InternetExplorerDriver
+	String expectedTitle = "Gmail";
 	@Test
 	public void launchApplication()
 	{
@@ -15,7 +18,28 @@ public class GmailAutomation {
 		ChromeDriver driver = new ChromeDriver();//Inside the constructor they wrote small code to open
 		//an empty google chrome browser by executing the chrome.exe file
 		driver.get("https://gmail.com");
-		
+		String actualTitle = driver.getTitle();
+		System.out.println("the current title :"+actualTitle);
+		Assert.assertEquals(expectedTitle, actualTitle);
+		String session = driver.getWindowHandle();
+		System.out.println("Session ID :"+session);		
+		String c_url = driver.getCurrentUrl();
+		System.out.println("Current URL :" + c_url);
+		System.out.println("My Current URL :" + driver.getCurrentUrl());
+		//Validation (or) Assertion
+		if(expectedTitle.equals(actualTitle))
+		{
+			System.out.println("Title Verifcation is PASSED");
+		}
+		else
+		{
+			System.out.println("Title Validation FAILED");
+		}
+		//Assert myassert = new Assert();		
+		Assert.assertEquals("My Title Validation is Failed", expectedTitle, actualTitle);
 	}
-
+	public void loginToApplication()
+	{
+		//How to Enter my username  - Object Identification
+	}
 }
