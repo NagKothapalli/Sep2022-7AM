947fd91b3e179202570ec676f0de88d5a5a62d02
NagKothapalli <nag022@gmail.com>
2022-10-04 08:32:13 +0530

    Oct4th code pushed

--- src/seleniumAutomation/ApsrtcAutomation.java
+++ src/seleniumAutomation/ApsrtcAutomation.java
@@ -1,6 +1,9 @@
 package seleniumAutomation;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -27,6 +30,39 @@ public class ApsrtcAutomation {
 	{
 		System.out.println("Launch Application");
 		driver.get("https://www.apsrtconline.in/");
+	}
+	//Actions - mouseHover , scroll , double click , right click , drag and drop , click , sendKeys
+	@Test
+	public void specialMethodsInActions()
+	{
+		launchApplication();
+		WebElement sourceCity = driver.findElement(By.xpath("//input[@name='source']"));
+		Actions actions = new Actions(driver);
+		actions.moveToElement(sourceCity).click().sendKeys("HYDERABAD").pause(Duration.ofSeconds(2)).sendKeys(Keys.ENTER).doubleClick().contextClick().build().perform();
+		actions.dragAndDrop(sourceCity, sourceCity).build().perform();
+	}
+	//org.openqa.selenium.NoSuchSessionException: Session ID is null. Using WebDriver after calling quit()?
+	@Test
+	public void handleMultipleWindows()
+	{
+		launchApplication();
+		driver.findElement(By.xpath("//a[@title='TimeTable / Track']")).click();
+		driver.findElement(By.xpath("//a[text()='All services Time Table & Tracking']")).click();
+		Set<String> windows = driver.getWindowHandles();
+		List<String> mywindows = new ArrayList<String>(windows);
+		for(int i=0;i<mywindows.size();i++)
+		{
+			System.out.println("SessionID of the widows :" + mywindows.get(i));			
+		}
+		System.out.println("Title 1st :" + driver.getTitle());
+		driver.switchTo().window(mywindows.get(1)); // 0 , 1
+		System.out.println("Title 2nd :" + driver.getTitle());
+		driver.close();	//Current active window only will be closed
+		//driver.quit(); //Will kill the complete driver process , all windows opened by the current driver instance will be closed
+		driver.switchTo().window(mywindows.get(0));
+		driver.findElement(By.xpath("//a[text()='Home']")).click();
+		//driver.close(); //Current active window only will be closed
+		driver.quit();
 	}
 	@Test
 	public void bookBusTicket() throws InterruptedException
